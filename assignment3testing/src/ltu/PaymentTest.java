d9fd46b559d9f482c0cf96c1b01f672d87777b96
ZEROZ0NE <melle.2014@hotmail.com>
2020-09-28 16:52:30 +0200

    så

--- assignment3testing/src/ltu/PaymentTest.java
+++ assignment3testing/src/ltu/PaymentTest.java
@@ -19,8 +19,12 @@ public class PaymentTest
             e.printStackTrace();
         }
     }
+    private static int studenloan = 7088;
+    private static int subsidiary =2816;
+    private static int studenloanhalf = 3564;
+    private static int subsidiaryhalf = 1396;
 
-
+    //AGE REQUIRMENTS
     @Test
     public void age19()
     {
@@ -32,5 +36,85 @@ public class PaymentTest
             System.out.println(testloan);
         }
     }
+    @Test
+    public void age20()
+    {
+        int testloan = -1;
+        try {
+            testloan = testobjekt.getMonthlyAmount("19960101-6969", 0, 100, 100);
+            assertEquals(studenloan + subsidiary, testloan);
+        }catch(AssertionError err){
+            System.out.println(testloan);
+        }
+    }
+    @Test
+    public void age46()
+    {
+        int testloan = -1;
+        try {
+            testloan = testobjekt.getMonthlyAmount("19700101-6969", 0, 100, 100);
+            assertEquals(studenloan + subsidiary, testloan);
+        }catch(AssertionError err){
+            System.out.println(testloan);
+        }
+    }
+    @Test
+    public void age47()
+    {
+        int testloan = -1;
+        try {
+            testloan = testobjekt.getMonthlyAmount("19690101-6969", 0, 100, 100);
+            assertEquals(subsidiary, testloan);
+        }catch(AssertionError err){
+            System.out.println(testloan);
+        }
+    }
+
+    @Test
+    public void age55()
+    {
+        int testloan = -1;
+        try {
+            testloan = testobjekt.getMonthlyAmount("19610101-6969", 0, 100, 100);
+            assertEquals(subsidiary, testloan);
+        }catch(AssertionError err){
+            System.out.println(testloan);
+        }
+    }
+    @Test
+    public void age56()
+    {
+        int testloan = -1;
+        try {
+            testloan = testobjekt.getMonthlyAmount("19600101-6969", 0, 100, 100);
+            assertEquals(0, testloan);
+        }catch(AssertionError err){
+            System.out.println(testloan);
+        }
+    }
+    // STUDYPACE REQUIRMENTS
+    @Test
+    public void rate100()
+    {
+        int testloan = -1;
+        try {
+            testloan = testobjekt.getMonthlyAmount("19930101-6969", 0, 100, 100);
+            assertEquals(studenloan + subsidiary, testloan);
+        }catch(AssertionError err){
+            System.out.println(testloan);
+        }
+    }
+    @Test
+    public void rateunder100()
+    {
+        int testloan = -1;
+        try {
+            testloan = testobjekt.getMonthlyAmount("19930101-6969", 0, 99, 100);
+            assertEquals(studenloanhalf + subsidiaryhalf, testloan);
+        }catch(AssertionError err){
+            System.out.println(testloan);
+        }
+    }
+
 
 }
--- assignment3testing/unittest.sh
+++ assignment3testing/unittest.sh
@@ -1,4 +1,4 @@
 echo "Running unittests..."
-java -javaagent:./lib/jacocoagent.jar -cp ./lib/org.junit4-4.3.1.jar:./bin org.junit.runner.JUnitCore ltu.PaymentTest
+java -javaagent:./lib/jacocoagent.jar -cp ./lib/org.junit4-4.3.1.jar:./bin/ltu/production/assignment3testing org.junit.runner.JUnitCore ltu.PaymentTest
 echo "Generating report..."
 java -jar ./lib/jacococli.jar report ./jacoco.exec --classfiles ./bin --html ./coveragereport --name CodeCoverageReport --sourcefiles ./src
